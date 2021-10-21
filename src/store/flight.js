22b8e4bac0692b9bb916e88f619fca97fdad37ea
Ashrith Krishna <ashrithuk93@gmail.com>
2021-10-21 11:17:21 +0530

    after complete setup

--- src/store/flight.js
+++ src/store/flight.js
@@ -4,9 +4,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const flightSlice = createSlice({
   name: "flight",
   initialState:
-    JSON.parse(localStorage.getItem("data")) === null
+    JSON.parse(localStorage.getItem("flight_data")) === null
       ? data
-      : JSON.parse(localStorage.getItem("data")),
+      : JSON.parse(localStorage.getItem("flight_data")),
   reducers: {
     checkIn(state, action) {
       const flightId = state.findIndex(
@@ -19,7 +19,7 @@ const flightSlice = createSlice({
       if (state[flightId].passengers[passenger].name.length !== 0) {
         state[flightId].passengers[passenger].checkedIn = !action.payload.check;
       }
-      localStorage.setItem("data", JSON.stringify(state));
+      localStorage.setItem("flight_data", JSON.stringify(state));
     },
     deletePassenger(state, action) {
       const flightId = state.findIndex(
@@ -36,7 +36,7 @@ const flightSlice = createSlice({
       state[flightId].passengers[passenger].disabled = false;
       state[flightId].passengers[passenger].infant = false;
 
-      localStorage.setItem("data", JSON.stringify(state));
+      localStorage.setItem("flight_data", JSON.stringify(state));
     },
     editPassenger(state, action) {
       const flightId = state.findIndex((item) => item.id === action.payload.id);
@@ -54,7 +54,7 @@ const flightSlice = createSlice({
         action.payload.wheelChairValue;
       state[flightId].passengers[passenger].infant = action.payload.infantValue;
 
-      localStorage.setItem("data", JSON.stringify(state));
+      localStorage.setItem("flight_data", JSON.stringify(state));
     },
   },
 });
@@ -62,74 +62,3 @@ const flightSlice = createSlice({
 export const flightActions = flightSlice.actions;
 
 export default flightSlice;
-
-// export const flightReducer = (
-//   state = JSON.parse(localStorage.getItem("data")) === null
-//     ? data
-//     : JSON.parse(localStorage.getItem("data")),
-//   action
-// ) => {
-//   if (action.type === "DELETE_PASSENGER") {
-//     const flightId = state.findIndex((item) => item.id === action.payload.fid);
-//     const passenger = state[flightId].passengers.findIndex(
-//       (item, index) => index === action.payload.pid
-//     );
-
-//     let updatedList = [...state];
-
-//     updatedList[flightId].passengers[passenger].name = "";
-//     updatedList[flightId].passengers[passenger].age = "";
-//     updatedList[flightId].passengers[passenger].address = "";
-//     updatedList[flightId].passengers[passenger].passportNo = "";
-//     updatedList[flightId].passengers[passenger].ancillary = "";
-//     updatedList[flightId].passengers[passenger].disabled = false;
-//     updatedList[flightId].passengers[passenger].infant = false;
-
-//     localStorage.setItem("data", JSON.stringify(updatedList));
-
-//     return updatedList;
-//   }
-
-//   if (action.type === "EDIT_PASSENGER") {
-//     const flightId = state.findIndex((item) => item.id === action.payload.id);
-//     const passenger = state[flightId].passengers.findIndex(
-//       (item) => item.seat === action.payload.seatNumber
-//     );
-
-//     let updatedList = [...state];
-
-//     updatedList[flightId].passengers[passenger].name = action.payload.name;
-//     updatedList[flightId].passengers[passenger].age = action.payload.age;
-//     updatedList[flightId].passengers[passenger].address =
-//       action.payload.address;
-//     updatedList[flightId].passengers[passenger].passportNo =
-//       action.payload.passport;
-//     updatedList[flightId].passengers[passenger].ancillary =
-//       action.payload.services;
-//     updatedList[flightId].passengers[passenger].disabled =
-//       action.payload.wheelChairValue;
-//     updatedList[flightId].passengers[passenger].infant =
-//       action.payload.infantValue;
-
-//     localStorage.setItem("data", JSON.stringify(updatedList));
-
-//     return updatedList;
-//   }
-
-//   if (action.type === "CHECK_IN") {
-//     const flightId = state.findIndex((item) => item.id === action.payload.fid);
-//     const passenger = state[flightId].passengers.findIndex(
-//       (item) => item.seat === action.payload.seat
-//     );
-
-//     let updatedList = [...state];
-//     if (updatedList[flightId].passengers[passenger].name.length !== 0) {
-//       updatedList[flightId].passengers[passenger].checkedIn =
-//         !action.payload.check;
-//     }
-//     localStorage.setItem("data", JSON.stringify(updatedList));
-//     return updatedList;
-//   }
-
-//   return state;
-// };
